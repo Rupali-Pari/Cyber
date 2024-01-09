95c9967830286bae61420855ec0f84b4c5151d87
Rupali <Rupali@DESKTOP-PNHRBD3>
2024-01-09 15:01:48 +0530

    COMMIT

---
+++ JavaSelenium/src/Pkg1/vehical.java
@@ -0,0 +1,13 @@
+package Pkg1;
+
+public class vehical {
+	 int price;
+	 String HondaCity;
+
+	public static void main(String[] args) {
+		System.out.println("price");
+		System.out.println("city");
+
+	}
+
+}
